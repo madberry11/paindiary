8407202221549043e9a6488de93a4a9abffb4b6b
madberry11 <1407067@rgu.ac.uk>
2016-02-22 22:40:56 +0000

    adding home.php

---
+++ home.php
@@ -0,0 +1,341 @@
+<?php
+
+ob_start();
+session_start();
+require ('config.inc.php'); 
+
+if (!isset($page_title)) {
+	$page_title = 'Your Pain Diary';
+
+}
+
+?>
+<!DOCTYPE>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title><?php echo $page_title; ?></title>
+    
+<!-- stylesheets -->
+	<link rel="stylesheet" href="style.css" type="text/css" />
+    <link rel="stylesheet" href="clndr.css">
+
+<!-- JavaScript / JQuery -->
+	<script type="text/javascript" src="jquery.mobile/jquery.mobile-1.4.5.js"></script>
+	<script type="text/javascript" src="script.js"></script>
+    <script src="json2.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  	<script src="http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min.js"></script>
+    <script src="moment-2.8.3.js"></script>
+  	<script src="clndr.js"></script>
+ 	<script src="site.js"></script>
+
+<!-- fonts -->
+	<link href='https://fonts.googleapis.com/css?family=Lato:400,900' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Ruda:700' rel='stylesheet' type='text/css'>
+
+
+<script type="text/javascript">
+var myInterval = setTimeout("location=('index.php');",3600000);
+</script>  
+  
+</head>
+
+<body>
+
+<div id="header">
+<div id="title"><a href="home.php" class="nounderline"><span class="lato900">Your</span> <span class="lato300">Pain Diary</span></a></div>
+<ul id="navbar">
+<li class="activetab"><a href="home.php" class="nounderline">Home</a></li>
+<li><a href="" class="nounderline">Profile</a></li>
+<li><a data-ajax="false" href="logout.php" class="nounderline">Logout</div></li>
+</ul>
+</div>
+<br clear="both" />
+<div id="divider"></div>
+<div id="pagewrap">
+
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "booksapp";
+
+$dbc = new mysqli($servername, $username, $password, $dbname);
+
+if (isset($_SESSION['keeploggedin'])) {
+	?>
+	<script type="text/javascript">
+	clearTimeout(myInterval);
+	</script>
+    <?php
+}
+
+if (isset($_GET['active'])) {
+$_SESSION['active'] = $_GET['active'];
+$active = $_SESSION['active'];
+
+if ($_GET['active'] == "allpain") {
+$whichquery = "allpain";}
+else { $whichquery = "bodypart";}
+// echo $active;
+}
+
+elseif ((!isset($_GET['active'])) AND (isset($_SESSION['active']))) {
+$_SESSION['active'] = $_SESSION['active'];
+$active = $_SESSION['active'];
+
+if ($_SESSION['active'] == "allpain") {
+$whichquery = "allpain";}
+else { $whichquery = "bodypart";}
+// echo $active;
+}
+
+else { 
+$active='allpain';
+// echo $active;
+$whichquery = "allpain";
+}
+
+
+if (isset($_SESSION['calmonth'])) {
+	$calmonth = $_SESSION['calmonth'];
+	$calyear = $_SESSION['calyear'];
+}
+
+if(isset($_GET['calday'])) {
+     $day = $_GET['calday'] - $_GET['startday'] + 1;
+	 $_SESSION['day'] = $_GET['calday'] - $_GET['startday'] + 1;
+}
+
+if (isset($_SESSION['first_name'])) { //Session based on correct First Name
+	//echo "{$_SESSION['first_name']}";
+}
+?>
+<!--<h1>Calendar</h1>-->
+
+<?php
+$monthNames = Array("January", "February", "March", "April", "May", "June", "July", 
+"August", "September", "October", "November", "December");
+?>
+<?php
+if (!isset($_REQUEST["month"])) $_REQUEST["month"] = date("n");
+if (!isset($_REQUEST["year"])) $_REQUEST["year"] = date("Y");
+?>
+<?php
+$cMonth = $_REQUEST["month"];
+$cYear = $_REQUEST["year"];
+
+$sql = "SELECT * FROM pain WHERE user_id="  . $_SESSION['user_id'] . " AND entryyear = " . $cYear . " AND entrymonth = " . $cMonth ;
+$result = $dbc->query($sql);
+if ($result -> num_rows > 0) {
+	
+	
+     // output data of each row
+     while($row = $result->fetch_assoc()) {
+		 //echo $row['entryid'];
+	 }
+}
+else {
+?>
+<script type="text/javascript">
+window.onload = function() {
+document.getElementById('allpain').style.display = "none";
+}
+</script>
+<?php
+}
+ 
+$prev_year = $cYear;
+$next_year = $cYear;
+$prev_month = $cMonth-1;
+$next_month = $cMonth+1;
+ 
+if ($prev_month == 0 ) {
+    $prev_month = 12;
+    $prev_year = $cYear - 1;
+}
+if ($next_month == 13 ) {
+    $next_month = 1;
+    $next_year = $cYear + 1;
+}
+?>
+<div id="monthnav">
+<a data-ajax="false" href="<?php echo $_SERVER["PHP_SELF"] . "?month=". $prev_month . "&year=" . $prev_year; ?>" class="nounderline"><div class="monthnavcell" id="previous"> Previous</div></a>
+<div class="monthnavcell" id="thismonth">
+<?php echo $monthNames[$cMonth-1].' '.$cYear; ?>
+</div>
+<a data-ajax="false" href="<?php echo $_SERVER["PHP_SELF"] . "?month=". $next_month . "&year=" . $next_year; ?>" class="nounderline"><div class="monthnavcell" id="next">Next</div></a>
+</div>
+
+
+<div id="monthlytags">
+<ul id="monthly">
+<?php
+$sql = "SELECT *, COUNT(entryid) FROM pain WHERE user_id="  . $_SESSION['user_id'] . " AND entryyear = " . $cYear . " AND entrymonth = " . $cMonth ;
+$result = $dbc->query($sql);
+if ($result -> num_rows > 0) {
+
+	while($row = $result->fetch_assoc()) {
+echo "<a data-ajax='false' onclick='allfunc()' href='home.php?active=allpain&month=$cMonth&year=$cYear'><li id='allpain'> all entries (". $row['COUNT(entryid)'] .")</li></a>";
+	}
+$sql2 = "SELECT bodypart, COUNT(entryid) FROM pain WHERE user_id="  . $_SESSION['user_id'] . " AND entryyear = " . $cYear . " AND entrymonth = " . $cMonth . " GROUP BY bodypart";
+$result2 = $dbc->query($sql2);
+if ($result2 -> num_rows > 0) {
+	while($row = $result2->fetch_assoc()) {
+		$bodypart = $row['bodypart'];
+echo "<a data-ajax='false' href='home.php?active=$bodypart&month=$cMonth&year=$cYear'><li id='".$bodypart."'>" . $row['bodypart'] . " (". $row['COUNT(entryid)'] .")</li></a>";
+if ($bodypart == $active) {
+	?>
+    <script type="text/javascript">
+	document.getElementById("<?php echo $bodypart ?>").className = "selected";
+	</script>
+    <?php
+}
+
+if ($active == 'allpain' ) {
+?>
+    <script type="text/javascript">
+	document.getElementById("allpain").className = "selected";
+	</script>
+    <?php	
+}
+
+	?>
+    <script type="text/javascript"> 
+	//document.getElementById('<?php echo $bodypart ?>').style.display = "none";
+	function chooseactive() {
+		window.location.reload();
+	}
+	
+	var choose = document.querySelectorAll('.chooseactive');
+for (var i = 0; i < choose.length; i++) {
+    choose[i].addEventListener('click', function(event) {
+		window.location.reload();
+        }
+    });
+}
+	</script>   
+    <?php
+}
+}
+}
+
+?>
+</ul>
+</div>
+<table id="calendar">
+<tr>
+<th>S</th>
+<th>M</th>
+<th>T</th>
+<th>W</th>
+<th>T</th>
+<th>F</th>
+<th>S</th>
+</tr>
+<?php 
+$sql = "SELECT * FROM pain WHERE user_id="  . $_SESSION['user_id'] . " AND entryyear = " . $cYear . " AND entrymonth = " . $cMonth ;
+$result = $dbc->query($sql);
+$timestamp = mktime(0,0,0,$cMonth,1,$cYear);
+$maxday = date("t",$timestamp);
+$thismonth = getdate ($timestamp);
+$startday = $thismonth['wday'];
+for ($i=0; $i<($maxday+$startday); $i++) {
+	
+		$thisday = $i - $startday + 1;
+	
+
+switch($whichquery) {
+		
+case 'allpain':
+	//echo " case all entries";
+	$sql = "SELECT entryday, AVG(avgpain) FROM pain WHERE user_id="  . $_SESSION['user_id'] . " AND entryyear = " . $cYear . " AND entrymonth = " . $cMonth ." AND entryday =" . $thisday  ;
+	$result = $dbc->query($sql);
+
+	break;
+case 'bodypart':
+	 //echo " case " . $active;
+	 if(!isset($_SESSION['active'])) {
+		 echo "It is really not set.";
+	 }
+	$sql2 = "SELECT entryday, AVG(avgpain) FROM pain WHERE user_id="  . $_SESSION['user_id'] . " AND entryyear = " . $cYear . " AND entrymonth = " . $cMonth ." AND entryday =" . $thisday . " AND bodypart = '" . $_SESSION['active'] . "'" ;
+	 //echo $sql2;
+	$result = mysqli_query ($dbc, $sql2) or trigger_error("Query: $sql2\n<br />MySQL Error: " . mysqli_error($dbc));
+	break;
+	
+	
+default:
+	//echo " case default";
+	$sql = "SELECT entryday, AVG(avgpain) FROM pain WHERE user_id="  . $_SESSION['user_id'] . " AND entryyear = " . $cYear . " AND entrymonth = " . $cMonth ." AND entryday =" . $thisday  ;
+	$result = $dbc->query($sql);
+	break;
+}
+
+	if ($result == FALSE) {
+	 //echo " no problem";
+	}
+	
+	elseif ($result -> num_rows > 0) {
+     while($row = $result->fetch_assoc()) {
+		 // if there's an entry for this day
+		 if ($row['entryday'] == $thisday) {
+			 
+			 if ($row['AVG(avgpain)'] < 0.95) {
+			 $thisday = "<div class='daynumber'>" . $thisday . "</div><div class='iconshere l1'>" . number_format($row['AVG(avgpain)'],1) ."</div>";
+			 }
+			elseif (($row['AVG(avgpain)'] >= 0.95) && ($row['AVG(avgpain)'] < 1.95)) {
+			 $thisday = "<div class='daynumber'>" . $thisday . "</div><div class='iconshere l2'>" . number_format($row['AVG(avgpain)'],1) ."</div>";
+			}
+			elseif (($row['AVG(avgpain)'] >= 1.95) && ($row['AVG(avgpain)'] < 2.95)) {
+			 $thisday = "<div class='daynumber'>" . $thisday . "</div><div class='iconshere l3'>" . number_format($row['AVG(avgpain)'],1) ."</div>";
+			}
+			elseif (($row['AVG(avgpain)'] >= 2.95) && ($row['AVG(avgpain)'] < 3.95)) {
+			 $thisday = "<div class='daynumber'>" . $thisday . "</div><div class='iconshere l4'>" . number_format($row['AVG(avgpain)'],1) ."</div>";
+			}
+			elseif (($row['AVG(avgpain)'] >= 3.95) && ($row['AVG(avgpain)'] < 4.95)) {
+			 $thisday = "<div class='daynumber'>" . $thisday . "</div><div class='iconshere l5'>" . number_format($row['AVG(avgpain)'],1) ."</div>";
+			}
+			elseif (($row['AVG(avgpain)'] >= 4.95) && ($row['AVG(avgpain)'] < 5.95)) {
+			 $thisday = "<div class='daynumber'>" . $thisday . "</div><div class='iconshere l6'>" . number_format($row['AVG(avgpain)'],1) ."</div>";
+			}
+			elseif (($row['AVG(avgpain)'] >= 5.95) && ($row['AVG(avgpain)'] < 6.95)) {
+			 $thisday = "<div class='daynumber'>" . $thisday . "</div><div class='iconshere l7'>" . number_format($row['AVG(avgpain)'],1) ."</div>";
+			}
+			elseif (($row['AVG(avgpain)'] >= 6.95) && ($row['AVG(avgpain)'] < 7.95)) {
+			 $thisday = "<div class='daynumber'>" . $thisday . "</div><div class='iconshere l8'>" . number_format($row['AVG(avgpain)'],1) ."</div>";
+			}
+			elseif (($row['AVG(avgpain)'] >= 7.95) && ($row['AVG(avgpain)'] < 8.95)) {
+			 $thisday = "<div class='daynumber'>" . $thisday . "</div><div class='iconshere l9'>" . number_format($row['AVG(avgpain)'],1) ."</div>";
+			}
+			elseif (($row['AVG(avgpain)'] >= 8.95) && ($row['AVG(avgpain)'] <= 10)) {
+			 $thisday = "<div class='daynumber'>" . $thisday . "</div><div class='iconshere l10'>" . number_format($row['AVG(avgpain)'],1) ."</div>";
+			}
+			
+		 }
+				 
+	 }
+}
+?>
+<?php
+	
+    if(($i % 7) == 0 )  {echo "<tr>"; }
+    if($i < $startday) {echo "<td></td>";}
+	
+	
+    // for cells in the table that have days in them
+    else { echo "<td class='actualday'><a data-ajax='false' href='newentry.php?calday=$i&startday=$startday'  class='nounderline'><div class='daynumber'>" . $thisday . "</div></a></td>"; }
+    if(($i % 7) == 6 ) echo "</tr>";
+}
+?>
+</tr>
+</table>
+<!--<div class="cal1">
+    </div>-->
+</div>
+<?php
+$_SESSION['calmonth'] = $cMonth;
+$_SESSION['calyear'] = $cYear;
+?>
+</body>
+</html>
