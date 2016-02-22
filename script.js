b6719e5e2580e8a7d36e56b9d23ae5b2160d8bab
madberry11 <1407067@rgu.ac.uk>
2016-02-22 22:52:24 +0000

    adding script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {// JavaScript Document
+
+$("#addnewbutton").click(function(){
+	$("#addnewbutton").css("display", "none");
+	$("#addnew").css("display", "block");
+});
+
+});
