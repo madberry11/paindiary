0bfd43c29f624f947701d53906b21ed07d977096
madberry11 <1407067@rgu.ac.uk>
2016-02-23 00:29:25 +0000

    changing mysql settings

--- mysqlconnect.php
+++ mysqlconnect.php
@@ -1,5 +1,5 @@
 <?php
-$dbc = mysqli_connect("127.0.0.1", "root", "", "booksapp", "3306") //Connect to your Database (Localhost IP, Username, Password, Databasename, LocalPort)
+$dbc = mysqli_connect("ap-cdbr-azure-east-c.cloudapp.net", "bcac3dbe9c1d06", "32d91723", "booksapp", "3306") //Connect to your Database (Localhost IP, Username, Password, Databasename, LocalPort)
 OR die ('Could not connect to MySQL: ' . mysqli_connect_error() );
 
 
