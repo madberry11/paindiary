334ab0f25ab7700cd2c171222ab67d8945604d08
madberry11 <1407067@rgu.ac.uk>
2016-03-02 16:23:47 +0000

    query

--- newentry.php
+++ newentry.php
@@ -251,7 +251,7 @@ if(isset($_GET['important'])) {
 	echo "important";
 	$query = "SELECT important_id FROM important WHERE entryyear=". $_SESSION['calyear'] ." AND entrymonth=". $_SESSION['calmonth'] ." AND entryday=". $_SESSION['day'] ." AND user_id=". $_SESSION['user_id'];
 	mysqli_query($dbc,$query) or die(mysqli_error($dbc));
-	$result = $dbc->query($sql);
+	$result = $dbc->query($query);
 	
 	// if this day had not been marked as important --> make important
 	if ($result -> num_rows == 0) {
