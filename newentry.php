f0c57857e23510cc0ee710a8ed6a0e791c7d306a
madberry11 <1407067@rgu.ac.uk>
2016-03-06 11:41:05 +0000

    knee!

--- newentry.php
+++ newentry.php
@@ -687,7 +687,7 @@ $(function () {
             categories: ['0AM', '1AM', '2AM', '3AM', '4AM', '5AM', '6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM', '9PM', '10PM', '11PM']
         },
         series: [{
-            name: '[<?php echo $bodypart[0] ?>]',
+            name: '<?php echo $bodypart[0] ?>',
             data: [<?php echo join($p16, ',') ?>]
         }, {
             name: 'John',
