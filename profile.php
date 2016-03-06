c8502345742a3ba955817ff2ae31dc810148b6dc
madberry11 <1407067@rgu.ac.uk>
2016-03-06 19:41:33 +0000

    change order

--- profile.php
+++ profile.php
@@ -117,9 +117,9 @@ $q = "SELECT user_id, username, email, pass, registration_date FROM users WHERE 
 <table id="usertable">
 <tr><th>Username:</th><td class="right"> <?php echo $username ?></td><td><a data-ajax='false' class='icon-edit nounderline' href='profile.php?edit=$username'></a></td></tr>
 <tr><th>Email address:</th><td class="right"><?php echo $email ?></td><td><a data-ajax='false' class='icon-edit nounderline' href='profile.php?edit=$email'></a></td></tr>
+<tr><th>Password:</th><td class="right"><a data-ajax='false' href='profile.php?edit=$password'>Change Password</a></td><td><a data-ajax='false' class='icon-edit nounderline' href='profile.php?edit=$password'></a></td></tr>
 <tr><th>Registration date:</th><td class="right"><?php echo date_format($register, 'Y-m-d'); ?></td><td>&nbsp;</td></tr>
 <tr><th>Number of entries:</th><td class="right">&nbsp;</td><td>&nbsp;</td></tr>
-<tr><th>Password:</th><td class="right"><a data-ajax='false' href='profile.php?edit=$password'>Change Password</a></td><td><a data-ajax='false' class='icon-edit nounderline' href='profile.php?edit=$password'></a></td></tr>
 </table>
 <p>Delete Account</p>
 </div>
