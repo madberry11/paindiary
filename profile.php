662f1ec4fd5e4071ba55f69cdd0b3ffe15577a30
madberry11 <1407067@rgu.ac.uk>
2016-03-06 18:30:36 +0000

    nobutton

--- profile.php
+++ profile.php
@@ -117,9 +117,9 @@ $q = "SELECT user_id, username, email, pass, registration_date FROM users WHERE 
 <table id="usertable">
 <tr><th>Username:</th><td class="right"> <?php echo $username ?></td><td><a data-ajax='false' class='icon-edit nounderline' href='profile.php?edit=$username'></a></td></tr>
 <tr><th>Email address:</th><td class="right"><?php echo $email ?></td><td><a data-ajax='false' class='icon-edit nounderline' href='profile.php?edit=$email'></a></td></tr>
-<tr><th>Registration date:</th><td class="right"><?php echo date_format($register, 'Y-m-d'); ?></td><td></td></tr>
-<tr><th>Number of entries:</th><td class="right"></td><td></td></tr>
-<tr><td colspan="3"><div id="changepass"><a href="" class="nounderline">Change Password</a></div></td></tr>
+<tr><th>Registration date:</th><td class="right"><?php echo date_format($register, 'Y-m-d'); ?></td><td>&nbsp;</td></tr>
+<tr><th>Number of entries:</th><td class="right">&nbsp;</td><td>&nbsp;</td></tr>
+<tr><td>Password:</td><td>&nbsp;</td><td><a data-ajax='false' class='icon-edit nounderline' href='profile.php?edit=$password'></a></td></tr>
 </table>
 <p>Delete Account</p>
 </div>
