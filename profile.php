5264f33da3a8b96dab1170070c4efea9a6b65c9a
madberry11 <1407067@rgu.ac.uk>
2016-03-10 00:59:12 +0000

    no echo

--- profile.php
+++ profile.php
@@ -123,7 +123,7 @@ while($row2 = $r2->fetch_assoc()) {
 	$year = $row2['entryyear'];
 	$month = $row2['entrymonth'];
 	$day = $row2['entryday'];
-	echo $year . " " . $month . " " . $day;
+	//echo $year . " " . $month . " " . $day . " ";
 	$numofentries++;
 	
 }
@@ -136,7 +136,7 @@ while($row2 = $r2->fetch_assoc()) {
 <tr><th>Email address:</th><td class="right"><?php echo $email ?></td><td><a data-ajax='false' class='icon-edit nounderline' href='profile.php?edit=$email'></a></td></tr>
 <tr><th>Password:</th><td class="right"><a data-ajax='false' href='profile.php?edit=$password'>Change Password</a></td><td><a data-ajax='false' class='icon-edit nounderline' href='profile.php?edit=$password'></a></td></tr>
 <tr><th>Registration date:</th><td class="right"><?php echo date_format($register, 'Y-m-d'); ?></td><td>&nbsp;</td></tr>
-<tr><th>Number of entries:</th><td class="right">&nbsp;</td><td><?php echo $numofentries ?></td></tr>
+<tr><th>Number of entries:</th><td class="right"><?php echo $numofentries ?></td><td>&nbsp;</td></tr>
 </table>
 <p>Delete Account</p>
 </div>
