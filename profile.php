b805ab08cff4c793a618f5977ea5d7787b8159bc
madberry11 <1407067@rgu.ac.uk>
2016-03-10 00:47:58 +0000

    q2

--- profile.php
+++ profile.php
@@ -114,7 +114,7 @@ $q = "SELECT user_id, username, email, pass, registration_date FROM users WHERE 
 	$password=$row['pass'];
 	}
 	
-$g2 = "SELECT entryyear, entrymonth, entryday FROM pain WHERE user_id='".$_SESSION['user_id']."' GROUP BY entryyear, entrymonth, entryday";
+$q2 = "SELECT entryyear, entrymonth, entryday FROM pain WHERE user_id='".$_SESSION['user_id']."' GROUP BY entryyear, entrymonth, entryday";
 //$q2 = "SELECT COUNT(*) AS numofentries FROM pain WHERE user_id='".$_SESSION['user_id']."' GROUP BY entryyear, entrymonth, entryday";
 $r2 = mysqli_query ($dbc, $q2) or trigger_error("Query: $q2\n<br />MySQL Error: " . mysqli_error($dbc));
 //$result2 = mysqli_fetch_array($r2,MYSQLI_NUM);
