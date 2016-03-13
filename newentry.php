43b2faf6fbc4b502d649c82b934033c895528bc9
madberry11 <1407067@rgu.ac.uk>
2016-03-13 12:49:46 +0000

    thickinterval

--- newentry.php
+++ newentry.php
@@ -733,12 +733,12 @@ $(function () {
             }
         },
         xAxis: {
+			tickInterval: 24 * 3600 * 1000,
             categories: ['0AM', '1AM', '2AM', '3AM', '4AM', '5AM', '6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM', '9PM', '10PM', '11PM']
         },
 		
 		plotOptions: {
         column: {
-			pointRange: 24 * 3600 * 1000,
             point: {
                 events: {
 
