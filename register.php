6ac0f9b6341433b0ad3ff305d18d7ca355b0822f
madberry11 <1407067@rgu.ac.uk>
2016-03-16 23:19:33 +0000

    azure email

--- register.php
+++ register.php
@@ -111,6 +111,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 				echo '<h3>Thank you for registering! A confirmation email has been sent to your address. Please click on the link in that email in order to activate your account. Or click the link below:<br /><a href="'.BASE_URL . 'activate.php?x=' . urlencode($e) . "&y=$a".'">click here</a></h3>'; 
 			
+			/*
 				// SendGrid
 				
 				//require 'vendor/autoload.php';
@@ -123,6 +124,68 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
       			->setHtml("and easy to do anywhere, even with PHP");
 
 				$sendgrid->send($email);
+				
+				*/
+				
+ include_once "vendor/autoload.php";
+ /*
+  * Create the body of the message (a plain-text and an HTML version).
+  * $text is your plain-text email
+  * $html is your html version of the email
+  * If the reciever is able to view html emails then only the html
+  * email will be displayed
+  */ 
+ $text = "Hi!\nHow are you?\n";
+ $html = "<html>
+       <head></head>
+       <body>
+           <p>Hi!<br>
+               How are you?<br>
+           </p>
+       </body>
+       </html>";
+ // This is your From email address
+ $from = array('someone@example.com' => 'Name To Appear');
+ // Email recipients
+ $to = array(
+       'nemrestellem@gmail.com'=>'Destination 1 Name',
+       '1407067@rgu.ac.uk'=>'Destination 2 Name'
+ );
+ // Email subject
+ $subject = 'Example PHP Email';
+
+ // Login credentials
+ $username = 'azure_2b39e0bb4e5f794200917155f6518255@azure.com';
+ $password = 'gugurekasu11';
+
+ // Setup Swift mailer parameters
+ $transport = Swift_SmtpTransport::newInstance('smtp.sendgrid.net', 587);
+ $transport->setUsername($username);
+ $transport->setPassword($password);
+ $swift = Swift_Mailer::newInstance($transport);
+
+ // Create a message (subject)
+ $message = new Swift_Message($subject);
+
+ // attach the body of the email
+ $message->setFrom($from);
+ $message->setBody($html, 'text/html');
+ $message->setTo($to);
+ $message->addPart($text, 'text/plain');
+ 
+ 
+ // send message 
+ if ($recipients = $swift->send($message, $failures))
+ {
+     // This will let us know how many users received this message
+     echo 'Message sent out to '.$recipients.' users';
+ }
+ // something went wrong =(
+ else
+ {
+     echo "Something went wrong - ";
+     print_r($failures);
+ }
 
 				
 				exit(); 
