3b3209bf87893fb94021f8637c79d85b194bb657
madberry11 <1407067@rgu.ac.uk>
2016-03-18 15:25:19 +0000

    sendmail

--- register.php
+++ register.php
@@ -114,39 +114,39 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			// Send Activation Email
 			
 
-require 'PHPMailer-master/PHPMailerAutoload.php';
- 
+/**
+ * This example shows sending a message using a local sendmail binary.
+ */
+
+require '../PHPMailer-master/PHPMailerAutoload.php';
+
+//Create a new PHPMailer instance
 $mail = new PHPMailer;
- 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';                       // Specify main and backup server
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'nemrestellem@gmail.com';                   // SMTP username
-$mail->Password = 'gugurekasu11';               // SMTP password
-$mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted
-$mail->Port = 587;                                    //Set the SMTP port number - 587 for authenticated TLS
-$mail->setFrom('nemrestellem@gmail.com', 'Your Pain Diary');     //Set who the message is to be sent from
-$mail->addAddress('1407067@rgu.ac.uk');               // Name is optional
-$mail->WordWrap = 50;                                 // Set word wrap to 50 characters
-//$mail->addAttachment('/usr/labnol/file.doc');         // Add attachments
-//$mail->addAttachment('/images/image.jpg', 'new.jpg'); // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
- 
-$mail->Subject = 'Here is the subject';
-$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
- 
+// Set PHPMailer to use the sendmail transport
+$mail->isSendmail();
+//Set who the message is to be sent from
+$mail->setFrom('nemrestellem@gmail.com', 'Your Pain Diary');
+//Set an alternative reply-to address
+$mail->addReplyTo('1407067@rgu.ac.uk', 'Your Pain Diary');
+//Set who the message is to be sent to
+$mail->addAddress('1407067@rgu.ac.uk');
+//Set the subject line
+$mail->Subject = 'PHPMailer sendmail test';
 //Read an HTML message body from an external file, convert referenced images to embedded,
 //convert HTML into a basic plain-text alternative body
 $mail->msgHTML(file_get_contents('PHPMailer-master/examples/contents.html'), dirname(__FILE__));
- 
-if(!$mail->send()) {
-   echo 'Message could not be sent.';
-   echo 'Mailer Error: ' . $mail->ErrorInfo;
-   exit;
+//Replace the plain text body with one created manually
+$mail->AltBody = 'This is a plain-text message body';
+//Attach an image file
+//$mail->addAttachment('images/phpmailer_mini.png');
+
+//send the message, check for errors
+if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message sent!";
 }
- 
-echo 'Message has been sent';
+
  
 				
 				exit(); 
