a7c424c0ac1dbc05ce68febc5db05a182b51a984
madberry11 <1407067@rgu.ac.uk>
2016-03-21 22:36:06 +0000

    hidden

--- profile.php
+++ profile.php
@@ -198,7 +198,7 @@ if(isset($_GET['edit'])) {
 	</select>
     </div>
     </td><td class="editcell">
-    <div id="save" class="hidden"><span class="icon-ok nounderline"><input type="submit" name="colour-submit" /></span></div>
+    <div id="save" class="hidden"><span class="icon-ok nounderline"><input class="hidden" type="submit" name="colour-submit" /></span></div>
     <div id="edit"><a data-ajax='false' class='icon-edit nounderline' href='profile.php?edit=colour'></a></div>
    	</td>
 </tr>
