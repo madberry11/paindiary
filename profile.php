cd05eb39b643fc29de21f0ed4c9c80a03ac13dea
madberry11 <1407067@rgu.ac.uk>
2016-03-21 23:36:08 +0000

    update

--- profile.php
+++ profile.php
@@ -50,7 +50,16 @@ if(isset($_COOKIE["unm"]) == $_SESSION["username"]) {
 }
 
 if (isset($_GET['colourIndex'])) {
-	echo "got back";
+	$colourIndex = $_GET['colourIndex'];
+	$q = "UPDATE users SET colour='". $colourIndex . "' WHERE user_id={$_SESSION['user_id']}";	
+		$r = mysqli_query ($dbc, $q) or trigger_error("Query: $q\n<br />MySQL Error: " . mysqli_error($dbc));
+		if (mysqli_affected_rows($dbc) == 1) {
+
+			
+			echo '<p class="success">Your preferred colour scheme has been changed.</p>';
+			mysqli_close($dbc);  
+			exit();
+		}
 }
 
 
