e3abcec9c9bca792b7799da9da0c80deebf20784
madberry11 <1407067@rgu.ac.uk>
2016-03-26 00:12:11 +0000

    prof2

--- profile.php
+++ profile.php
@@ -520,7 +520,7 @@ if (!empty($_POST['deleteaccount'])) {
 <legend>Change Username</legend>
 	<form action="profile.php" method="post">
     <table class="usertable">
-	<tr><th>Enter Old Username:</th><td class="userdata"><input class="logininput" type="text" name="username0" placeholder="Enter Old Username" maxlength="40" /></td><td class="editcell">&nbsp;</td></tr>
+	<tr><th>Enter Old Username:</th><td class="userdata"><input class="logininput prof2" type="text" name="username0" placeholder="Enter Old Username" maxlength="40" /></td><td class="editcell">&nbsp;</td></tr>
 	<tr><th>Enter New Username:</th><td class="userdata">something</td><td class="editcell">&nbsp;</a></td></tr>
 	<tr><th>Confirm New Username:</th><td class="userdata">something</td><td class="editcell">&nbsp;</td></tr>
     </table>
