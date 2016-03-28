e30a94819753c1d8d8bfcc7b30c7bce614e40f5a
madberry11 <1407067@rgu.ac.uk>
2016-03-28 21:53:07 +0100

    space

--- report.php
+++ report.php
@@ -226,7 +226,7 @@ if ($result -> num_rows > 0) {
 	
      // output data of each row
      while($row = $result->fetch_assoc()) {
-		 echo $row['entryid'];
+		 echo $row['entryid'] + " ";
 	 }
 }
 else {
