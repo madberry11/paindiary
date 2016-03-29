335b60a83ac4cfee410902715d8f298c82a57e9f
madberry11 <1407067@rgu.ac.uk>
2016-03-29 03:18:29 +0100

    showinlegend

--- report.php
+++ report.php
@@ -798,7 +798,7 @@ $(function () {
         },
 		yAxis: {
             title: {
-                text: 'Pain Itensity'
+                text: 'Average Pain Itensity'
             }
         },
         xAxis: {
@@ -814,7 +814,7 @@ $(function () {
 		
         series: [
 		{
-
+			showInLegend: false, 
             name: 'Monthly Pain Intensity',
 			data: [<?php echo $day1. ',' .$day2. ',' .$day3. ',' .$day4. ',' .$day5. ',' .$day6. ',' .$day7. ',' .$day8. ',' .$day9. ',' .$day10. ',' .$day11. ',' .$day12. ',' .$day13. ',' .$day14. ',' .$day15. ',' .$day16. ',' .$day17. ',' .$day18. ',' .$day19. ',' .$day20. ',' .$day21. ',' .$day22. ',' .$day23. ',' .$day24. ',' .$day25. ',' .$day26. ',' .$day27. ',' .$day28. ',' .$day29. ',' .$day30. ',' .$day31 ?>],
 			draggableY: true,
