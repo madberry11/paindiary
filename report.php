b53a0d2786a0eb6d9b59781a0ffcdf4feca3744e
madberry11 <1407067@rgu.ac.uk>
2016-03-29 01:41:47 +0100

    day18

--- report.php
+++ report.php
@@ -208,7 +208,7 @@ switch ($calmonth) {
 <div id="divider"></div>
 
 <div id="pagewrap">
-<h1>Report for <?php echo $month ?> <?php echo $calyear ?></h1>
+<h1><a href="home.php" class="icon-chevron-left nounderline"></a>Report for <?php echo $month ?> <?php echo $calyear ?></h1>
 <?php
 $servername = "ap-cdbr-azure-east-c.cloudapp.net";
 $username = "bcac3dbe9c1d06";
@@ -217,10 +217,15 @@ $dbname = "booksapp";
 
 $dbc = new mysqli($servername, $username, $password, $dbname);
 
+
+
+
 $sql = "SELECT * FROM pain WHERE user_id="  . $_SESSION['user_id'] . " AND entryyear = " . $calyear . " AND entrymonth = " . $calmonth;
 $result = $dbc->query($sql);
 if ($result -> num_rows > 0) {
 
+
+// Day 1
 $sql1 = "SELECT * FROM pain WHERE user_id="  . $_SESSION['user_id'] . " AND entryyear = " . $calyear . " AND entrymonth = " . $calmonth . " AND entryday = 1 ";
 $result1 = $dbc->query($sql1);
 if ($result1 -> num_rows > 0) {
@@ -228,19 +233,329 @@ if ($result1 -> num_rows > 0) {
 	 $day1num = 0;
 	 $day1sum = 0;
      while($row = $result->fetch_assoc()) {
-		 echo " &nbsp " . $row['avgpain'];
 		 $day1num++;
 		 $day1sum = $day1sum + $row['avgpain'];
 	 }
-	 //echo "number of entries: ". $day1num;
-	 //echo "sum of avgpain: ". $day1sum;
 	 $day1 = $day1sum/$day1num;
-	 //echo "day1 average: ". $day1;
+	 echo "day1 average: ". $day1 ."</br />";
 }
 else {
-	echo "There is nothing on the first.";
+	//echo "There is nothing on the 1st.";
 }
+
+// Day 2
+$sql2 = "SELECT * FROM pain WHERE user_id="  . $_SESSION['user_id'] . " AND entryyear = " . $calyear . " AND entrymonth = " . $calmonth . " AND entryday = 2 ";
+$result2 = $dbc->query($sql2);
+if ($result2 -> num_rows > 0) {
+	
+	 $day2num = 0;
+	 $day2sum = 0;
+     while($row = $result->fetch_assoc()) {
+		 $day2num++;
+		 $day2sum = $day2sum + $row['avgpain'];
+	 }
+	 $day2 = $day2sum/$day2num;
+	 echo "day2 average: ". $day2 ."</br />";
 }
+else {
+	//echo "There is nothing on the 2nd.";
+}
+
+// Day 3
+$sql3 = "SELECT * FROM pain WHERE user_id="  . $_SESSION['user_id'] . " AND entryyear = " . $calyear . " AND entrymonth = " . $calmonth . " AND entryday = 3 ";
+$result3 = $dbc->query($sql3);
+if ($result3 -> num_rows > 0) {
+	
+	 $day3num = 0;
+	 $day3sum = 0;
+     while($row = $result->fetch_assoc()) {
+		 $day3num++;
+		 $day3sum = $day3sum + $row['avgpain'];
+	 }
+	 $day3 = $day3sum/$day3num;
+	 echo "day3 average: ". $day3 ."</br />";
+}
+else {
+	//echo "There is nothing on the 3rd.";
+}
+
+// Day 4
+$sql4 = "SELECT * FROM pain WHERE user_id="  . $_SESSION['user_id'] . " AND entryyear = " . $calyear . " AND entrymonth = " . $calmonth . " AND entryday = 4 ";
+$result4 = $dbc->query($sql4);
+if ($result4 -> num_rows > 0) {
+	
+	 $day4num = 0;
+	 $day4sum = 0;
+     while($row = $result->fetch_assoc()) {
+		 $day4num++;
+		 $day4sum = $day4sum + $row['avgpain'];
+	 }
+	 $day4 = $day4sum/$day4num;
+	 echo "day4 average: ". $day4 ."</br />";
+}
+else {
+	//echo "There is nothing on the 4th.";
+}
+
+// Day 5
+$sql5 = "SELECT * FROM pain WHERE user_id="  . $_SESSION['user_id'] . " AND entryyear = " . $calyear . " AND entrymonth = " . $calmonth . " AND entryday = 5 ";
+$result5 = $dbc->query($sql5);
+if ($result5 -> num_rows > 0) {
+	
+	 $day5num = 0;
+	 $day5sum = 0;
+     while($row = $result->fetch_assoc()) {
+		 $day5num++;
+		 $day5sum = $day5sum + $row['avgpain'];
+	 }
+	 $day5 = $day5sum/$day5num;
+	 echo "day5 average: ". $day5 ."</br />";
+}
+else {
+	//echo "There is nothing on the 5th.";
+}
+
+// Day 6
+$sql6 = "SELECT * FROM pain WHERE user_id="  . $_SESSION['user_id'] . " AND entryyear = " . $calyear . " AND entrymonth = " . $calmonth . " AND entryday = 6 ";
+$result6 = $dbc->query($sql6);
+if ($result6 -> num_rows > 0) {
+	
+	 $day6num = 0;
+	 $day6sum = 0;
+     while($row = $result->fetch_assoc()) {
+		 $day6num++;
+		 $day6sum = $day6sum + $row['avgpain'];
+	 }
+	 $day6 = $day6sum/$day6num;
+	 echo "day6 average: ". $day6 ."</br />";
+}
+else {
+	//echo "There is nothing on the 6th.";
+}
+
+// Day 7
+$sql7 = "SELECT * FROM pain WHERE user_id="  . $_SESSION['user_id'] . " AND entryyear = " . $calyear . " AND entrymonth = " . $calmonth . " AND entryday = 7 ";
+$result7 = $dbc->query($sql7);
+if ($result7 -> num_rows > 0) {
+	
+	 $day7num = 0;
+	 $day7sum = 0;
+     while($row = $result->fetch_assoc()) {
+		 $day7num++;
+		 $day7sum = $day7sum + $row['avgpain'];
+	 }
+	 $day7 = $day7sum/$day7num;
+	 echo "day7 average: ". $day7 ."</br />";
+}
+else {
+	//echo "There is nothing on the 7th.";
+}
+
+// Day 8
+$sql8 = "SELECT * FROM pain WHERE user_id="  . $_SESSION['user_id'] . " AND entryyear = " . $calyear . " AND entrymonth = " . $calmonth . " AND entryday = 8 ";
+$result8 = $dbc->query($sql8);
+if ($result8 -> num_rows > 0) {
+	
+	 $day8num = 0;
+	 $day8sum = 0;
+     while($row = $result->fetch_assoc()) {
+		 $day8num++;
+		 $day8sum = $day8sum + $row['avgpain'];
+	 }
+	 $day8 = $day8sum/$day8num;
+	 echo "day8 average: ". $day8 ."</br />";
+}
+else {
+	//echo "There is nothing on the 8th.";
+}
+
+// Day 9
+$sql9 = "SELECT * FROM pain WHERE user_id="  . $_SESSION['user_id'] . " AND entryyear = " . $calyear . " AND entrymonth = " . $calmonth . " AND entryday = 9 ";
+$result9 = $dbc->query($sql9);
+if ($result9 -> num_rows > 0) {
+	
+	 $day9num = 0;
+	 $day9sum = 0;
+     while($row = $result->fetch_assoc()) {
+		 $day9num++;
+		 $day9sum = $day9sum + $row['avgpain'];
+	 }
+	 $day9 = $day9sum/$day9num;
+	 echo "day9 average: ". $day9 ."</br />";
+}
+else {
+	//echo "There is nothing on the 9th.";
+}
+
+// Day 10
+$sql10 = "SELECT * FROM pain WHERE user_id="  . $_SESSION['user_id'] . " AND entryyear = " . $calyear . " AND entrymonth = " . $calmonth . " AND entryday = 10 ";
+$result10 = $dbc->query($sql10);
+if ($result10 -> num_rows > 0) {
+	
+	 $day10num = 0;
+	 $day10sum = 0;
+     while($row = $result->fetch_assoc()) {
+		 $day10num++;
+		 $day10sum = $day10sum + $row['avgpain'];
+	 }
+	 $day10 = $day10sum/$day10num;
+	 echo "day10 average: ". $day10 ."</br />";
+}
+else {
+	//echo "There is nothing on the 10th.";
+}
+
+// Day 11
+$sql11 = "SELECT * FROM pain WHERE user_id="  . $_SESSION['user_id'] . " AND entryyear = " . $calyear . " AND entrymonth = " . $calmonth . " AND entryday = 11 ";
+$result11 = $dbc->query($sql11);
+if ($result11 -> num_rows > 0) {
+	
+	 $day11num = 0;
+	 $day11sum = 0;
+     while($row = $result->fetch_assoc()) {
+		 $day11num++;
+		 $day11sum = $day11sum + $row['avgpain'];
+	 }
+	 $day11 = $day11sum/$day11num;
+	 echo "day11 average: ". $day11 ."</br />";
+}
+else {
+	//echo "There is nothing on the 11th.";
+}
+
+// Day 12
+$sql12 = "SELECT * FROM pain WHERE user_id="  . $_SESSION['user_id'] . " AND entryyear = " . $calyear . " AND entrymonth = " . $calmonth . " AND entryday = 12 ";
+$result12 = $dbc->query($sql12);
+if ($result12 -> num_rows > 0) {
+	
+	 $day12num = 0;
+	 $day12sum = 0;
+     while($row = $result->fetch_assoc()) {
+		 $day12num++;
+		 $day12sum = $day12sum + $row['avgpain'];
+	 }
+	 $day12 = $day12sum/$day12num;
+	 echo "day12 average: ". $day12 ."</br />";
+}
+else {
+	//echo "There is nothing on the 12th.";
+}
+
+// Day 13
+$sql13 = "SELECT * FROM pain WHERE user_id="  . $_SESSION['user_id'] . " AND entryyear = " . $calyear . " AND entrymonth = " . $calmonth . " AND entryday = 13 ";
+$result13 = $dbc->query($sql13);
+if ($result13 -> num_rows > 0) {
+	
+	 $day13num = 0;
+	 $day13sum = 0;
+     while($row = $result->fetch_assoc()) {
+		 $day13num++;
+		 $day13sum = $day13sum + $row['avgpain'];
+	 }
+	 $day13 = $day13sum/$day13num;
+	 echo "day13 average: ". $day13 ."</br />";
+}
+else {
+	//echo "There is nothing on the 13th.";
+}
+
+// Day 14
+$sql14 = "SELECT * FROM pain WHERE user_id="  . $_SESSION['user_id'] . " AND entryyear = " . $calyear . " AND entrymonth = " . $calmonth . " AND entryday = 14 ";
+$result14 = $dbc->query($sql14);
+if ($result14 -> num_rows > 0) {
+	
+	 $day14num = 0;
+	 $day14sum = 0;
+     while($row = $result->fetch_assoc()) {
+		 $day14num++;
+		 $day14sum = $day14sum + $row['avgpain'];
+	 }
+	 $day14 = $day14sum/$day14num;
+	 echo "day14 average: ". $day14 ."</br />";
+}
+else {
+	//echo "There is nothing on the 14th.";
+}
+
+// Day 15
+$sql15 = "SELECT * FROM pain WHERE user_id="  . $_SESSION['user_id'] . " AND entryyear = " . $calyear . " AND entrymonth = " . $calmonth . " AND entryday = 15 ";
+$result15 = $dbc->query($sql15);
+if ($result15 -> num_rows > 0) {
+	
+	 $day15num = 0;
+	 $day15sum = 0;
+     while($row = $result->fetch_assoc()) {
+		 $day15num++;
+		 $day15sum = $day15sum + $row['avgpain'];
+	 }
+	 $day15 = $day15sum/$day15num;
+	 echo "day15 average: ". $day15 ."</br />";
+}
+else {
+	//echo "There is nothing on the 15th.";
+}
+
+// Day 16
+$sql16 = "SELECT * FROM pain WHERE user_id="  . $_SESSION['user_id'] . " AND entryyear = " . $calyear . " AND entrymonth = " . $calmonth . " AND entryday = 16 ";
+$result16 = $dbc->query($sql16);
+if ($result16 -> num_rows > 0) {
+	
+	 $day16num = 0;
+	 $day16sum = 0;
+     while($row = $result->fetch_assoc()) {
+		 $day16num++;
+		 $day16sum = $day16sum + $row['avgpain'];
+	 }
+	 $day16 = $day16sum/$day16num;
+	 echo "day16 average: ". $day16 ."</br />";
+}
+else {
+	//echo "There is nothing on the 16th.";
+}
+
+// Day 17
+$sql17 = "SELECT * FROM pain WHERE user_id="  . $_SESSION['user_id'] . " AND entryyear = " . $calyear . " AND entrymonth = " . $calmonth . " AND entryday = 17 ";
+$result17 = $dbc->query($sql17);
+if ($result17 -> num_rows > 0) {
+	
+	 $day17num = 0;
+	 $day17sum = 0;
+     while($row = $result->fetch_assoc()) {
+		 $day17num++;
+		 $day17sum = $day17sum + $row['avgpain'];
+	 }
+	 $day17 = $day17sum/$day17num;
+	 echo "day17 average: ". $day17 ."</br />";
+}
+else {
+	//echo "There is nothing on the 17th.";
+}
+
+// Day 18
+$sql18 = "SELECT * FROM pain WHERE user_id="  . $_SESSION['user_id'] . " AND entryyear = " . $calyear . " AND entrymonth = " . $calmonth . " AND entryday = 18 ";
+$result18 = $dbc->query($sql18);
+if ($result18 -> num_rows > 0) {
+	
+	 $day18num = 0;
+	 $day18sum = 0;
+     while($row = $result->fetch_assoc()) {
+		 $day18num++;
+		 $day18sum = $day18sum + $row['avgpain'];
+	 }
+	 $day18 = $day18sum/$day18num;
+	 echo "day18 average: ". $day18 ."</br />";
+}
+else {
+	//echo "There is nothing on the 18th.";
+}
+
+
+
+
+
+
+
+} // This closes the monthly overall SQL.
 
 else {
 	echo "There is nothing to report for this month yet.";
