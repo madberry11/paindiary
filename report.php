4748c699ec26db544545bb3716988104f0a70893
madberry11 <1407067@rgu.ac.uk>
2016-03-30 21:45:29 +0100

    while

--- report.php
+++ report.php
@@ -979,8 +979,13 @@ else {
     	foreach($_POST['bodypart'] as $check1) {
 			echo $check1 . "<br />";
 			$pain[] = $check1;
+			$countparts++;
 		}
 print_r($pain);
+$i = 0;
+while ($i < $countparts) {
+	
+}
 
 		} // close tag for if not empty
 	} // close tag for Pain Intensity Comparison
