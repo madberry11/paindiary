5bb9ba7ce3d04ceadddc0260f3cef668f505e366
madberry11 <1407067@rgu.ac.uk>
2016-03-30 22:15:51 +0100

    avgpain

--- report.php
+++ report.php
@@ -989,7 +989,7 @@ while ($i < $countparts) {
 	$r16 = mysqli_query ($dbc, $q16) or trigger_error("Query: $q16\n<br />MySQL Error: " . mysqli_error($dbc));
 	if (@mysqli_num_rows($r16) == 1) { 
 	$row = mysqli_fetch_assoc($r16);
-	$pl16 = $row['username'];
+	$pl16 = $row['avgpain'];
 	echo $pl16;
 	}
 	$i++;
