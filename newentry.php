fe09ff9df9fa9b2c7617fe5d8df93528ab7e1f77
madberry11 <1407067@rgu.ac.uk>
2016-04-06 12:50:30 +0100

    alert

--- newentry.php
+++ newentry.php
@@ -952,7 +952,7 @@ function Deleteqry() {
 	var r = confirm("Are you sure you want to delete this entry?");
 if (r == true) {
 	document.getElementById('todelete').click();
-	//alert("Gonna delete it!");
+	alert("Gonna delete it!");
 } else {
     x = "You pressed Cancel!";
 	document.getElementById('canceldeleteentry').click();
