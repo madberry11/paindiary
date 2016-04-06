68998da8a14bec7153c3238b8dd28fcf2254883e
madberry11 <1407067@rgu.ac.uk>
2016-04-06 12:32:37 +0100

    document.write

--- newentry.php
+++ newentry.php
@@ -368,7 +368,6 @@ if(isset($_GET['createcomment'])) {
 	
 if(isset($_GET['todelete'])) {
 	$idtodelete = mysqli_real_escape_string ($dbc, $_GET['todelete']);
-	echo "todelete";
 	$query = "DELETE FROM pain WHERE entryid = " . $idtodelete;
     $result = mysqli_query ($dbc, $query) or trigger_error("Query: $query\n<br />MySQL Error: " . mysqli_error($dbc));
 }
@@ -963,6 +962,7 @@ if (r == true) {
 function Deleterecordqry() { 
 	var r = confirm("Are you sure you want to delete this pain relief record?");
 if (r == true) {
+	document.write("Yes");
 	document.getElementById('deletepainrelief').click();
 } else {
     x = "You pressed Cancel!";
