9cf0e3f18fa517a9c8cefa6e8bb9bfa9a53a74bd
madberry11 <1407067@rgu.ac.uk>
2016-04-06 15:30:04 +0100

    redirect

--- newentry.php
+++ newentry.php
@@ -1715,11 +1715,11 @@ else {$p23 = mysqli_real_escape_string ($dbc, $trimmed['p23']);}
 			$r = mysqli_query ($dbc, $q) or trigger_error("Query: $q\n<br />MySQL Error: " . mysqli_error($dbc));
 
 					if (mysqli_affected_rows($dbc) == 1) {
-/*
+
 	$url = BASE_URL . 'newentry.php'; 
 	ob_end_clean(); 
 	header("Location: $url");
-*/
+
 						
 					}else {
 						echo '<p class="error">The entry could not be changed due to a system error. We apologize for any inconvenience.</p>';
