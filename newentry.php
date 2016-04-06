3ae44cc816a6676fed22bc9dbcc6ed15416dbc6b
madberry11 <1407067@rgu.ac.uk>
2016-04-06 11:43:15 +0100

    record_id

--- newentry.php
+++ newentry.php
@@ -962,7 +962,7 @@ if (r == true) {
 function Deleterecordqry() { 
 	var r = confirm("Are you sure you want to delete this pain relief record?");
 if (r == true) {
-	window.location="newentry.php?deleterecord=$row[entryid]";
+	window.location="newentry.php?deleterecord=$row[record_id]";
 	document.getElementById('deletepainrelief').click();
 } else {
     x = "You pressed Cancel!";
