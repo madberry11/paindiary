646c84839ccb550ad1b4c32ab4748ef76621ca01
madberry11 <1407067@rgu.ac.uk>
2016-04-07 14:08:16 +0100

    string

--- newentry.php
+++ newentry.php
@@ -367,7 +367,7 @@ if(isset($_GET['createcomment'])) {
 	
 if(isset($_GET['todelete'])) {
 	$idtodelete = mysqli_real_escape_string ($dbc, $_GET['todelete']);
-	$query = "DELETE FROM pain WHERE entryid = " . $idtodelete;
+	$query = "DELETE FROM pain WHERE entryid = '" . $idtodelete ."'";
     $result = mysqli_query ($dbc, $query) or trigger_error("Query: $query\n<br />MySQL Error: " . mysqli_error($dbc));
 }
 
