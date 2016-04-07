3928896f7bc8662e5c8fb1a91399cdd3ba6b0d55
madberry11 <1407067@rgu.ac.uk>
2016-04-07 18:49:35 +0100

    remove

--- newentry.php
+++ newentry.php
@@ -98,7 +98,7 @@ var myInterval = setTimeout("location=('index.php');",3600000);
             var string = "";
             var i;
             for (i in tags) {
-                string += tags[i].value.replace(/[_\W]+/g, "-") + ", ";
+                string += tags[i].value.replace(/[_\W]+/g, "") + ", ";
 				//string += "&nbsp;";
             }
 			$('#demo4Out').append(string);
